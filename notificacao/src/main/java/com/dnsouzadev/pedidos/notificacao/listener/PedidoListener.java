555c1252340dc762f0d5a9cb93561db983bbf53d
Daniel Souza <workdndsza@gmail.com>
2025-03-31 20:37:55 -0300

    testando o envio de email

--- notificacao/src/main/java/com/dnsouzadev/pedidos/notificacao/listener/PedidoListener.java
+++ notificacao/src/main/java/com/dnsouzadev/pedidos/notificacao/listener/PedidoListener.java
@@ -1,6 +1,7 @@
 package com.dnsouzadev.pedidos.notificacao.listener;
 
 import com.dnsouzadev.pedidos.notificacao.entity.Pedido;
+import com.dnsouzadev.pedidos.notificacao.service.EmailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -12,8 +13,20 @@ public class PedidoListener {
 
     private final Logger log = LoggerFactory.getLogger(PedidoListener.class);
 
+    private final EmailService emailService;
+
+    public PedidoListener(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @RabbitListener(queues = "${rabbitmq.queue.name}")
     public void enviarNotificacao(Pedido pedido) {
         log.info("Notificação gerada com sucesso! -> {}", pedido.toString());
+        try {
+            emailService.enviarEmail(pedido);
+            log.info("Email enviado com sucesso para o cliente: {}", pedido.getCliente());
+        } catch (Exception e) {
+            log.error("Erro ao enviar email para o cliente: {}", pedido.getCliente(), e);
+        }
     }
 }
